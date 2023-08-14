1479fd78145b6d67463c22813aa58da838ec5537
hieu <pmh.ars14@gmail.com>
2023-08-14 22:46:01 +0700

    (nvim) migrate to lazy.nvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,1 +1,2 @@
-require("k4ttitude")
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
--- .config/nvim/lua/k4ttitude/init.lua
+++
@@ -1,7 +0,0 @@
-require("k4ttitude.keymap")
-if vim.g.vscode then
-  -- vscode
-else
-  require("k4ttitude.packer")
-  require("k4ttitude.set")
-end
