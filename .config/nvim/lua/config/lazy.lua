52af31264a158330bc091c959a5503c50dc8e5ef
hieu <pmh.ars14@gmail.com>
2024-07-25 22:52:42 +0700

    (nvim): eslint prettier

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -16,15 +16,13 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.json" },
     -- { import = "lazyvim.plugins.extras.lang.tailwind" },
     { import = "lazyvim.plugins.extras.linting.eslint" },
-    -- { import = "lazyvim.plugins.extras.formatting.prettier" },
+    { import = "lazyvim.plugins.extras.formatting.prettier" },
+
     -- editor
     -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
     { import = "lazyvim.plugins.extras.editor.leap" },
-    -- copilot
-    -- { import = "lazyvim.plugins.extras.coding.copilot" },
     -- import/override with your plugins
     { import = "plugins" },
-    -- { import = ""}
   },
   defaults = {
     -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
--- .config/nvim/lua/plugins/nvim-lspconfig.lua
+++ .config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -3,16 +3,15 @@ return {
   opts = {
     servers = { eslint = {} },
     setup = {
-      -- eslint = function()
-      --   require("lazyvim.util").lsp.on_attach(function(client)
-      --     if client.name == "eslint" then
-      --       client.server_capabilities.documentFormattingProvider = true
-      --       return
-      --     elseif client.name == "tsserver" then
-      --       client.server_capabilities.documentFormattingProvider = false
-      --     end
-      --   end)
-      -- end,
+      eslint = function()
+        require("lazyvim.util").lsp.on_attach(function(client)
+          if client.name == "eslint" then
+            client.server_capabilities.documentFormattingProvider = true
+          elseif client.name == "vtsls" then
+            client.server_capabilities.documentFormattingProvider = false
+          end
+        end)
+      end,
     },
   },
 }
