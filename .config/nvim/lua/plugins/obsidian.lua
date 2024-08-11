558a103c6de6c062bdea13b3692622d6b63bdae9
hieu <pmh.ars14@gmail.com>
2024-08-11 21:53:02 +0700

    (nvim): obsidian, harpoon2

--- .config/nvim/lua/plugins/obsidian.lua
+++ .config/nvim/lua/plugins/obsidian.lua
@@ -1,7 +1,7 @@
 return {
   "epwalsh/obsidian.nvim",
   version = "*", -- recommended, use latest release instead of latest commit
-  lazy = true,
+  lazy = false,
   ft = "markdown",
   -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
   -- event = {
@@ -23,7 +23,8 @@ return {
         path = "~/Library/Mobile Documents/iCloud~md~obsidian/Documents",
       },
     },
-
-    -- see below for full list of options 👇
+  },
+  keys = {
+    { "<leader>sO", "<cmd>ObsidianSearch<cr>", desc = "Obsidian" },
   },
 }
